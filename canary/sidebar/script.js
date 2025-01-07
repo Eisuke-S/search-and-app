361fa0e5bb8afa30b59e9d0bade74b9c821ad151
鈴木英資 <151418756+Eisuke-S@users.noreply.github.com>
2025-01-07 13:06:40 +0900

    Add files via upload

--- canary/sidebar/script.js
+++ canary/sidebar/script.js
@@ -43,3 +43,39 @@ appButton.addEventListener("click", function(){
     const settings = document.getElementById("settings");
     settings.style.display = "none";
 });
+
+
+//タッチデバイス対応
+// タッチデバイス向けにイベントリスナーを追加
+sidebarButton.addEventListener('click', toggleSidebarVisibility);
+sidebarButton.addEventListener('touchstart', toggleSidebarVisibility);
+
+// サイドバーが閉じられたときにボタンを再表示（タッチイベントに対応）
+sidebar.addEventListener('focusout', (event) => {
+    // フォーカスがサイドバー内の子要素にある場合は処理を停止
+    if (sidebar.contains(event.relatedTarget)) {
+        return;  // 子要素から外れた場合のみ実行
+    }
+
+    // サイドバーを非表示にする
+    sidebar.style.transform = 'translateX(-100%)';
+    sidebarButton.style.opacity = '1';
+    sidebarButton.style.pointerEvents = 'auto';
+});
+
+
+// apps
+settingButton.addEventListener("click", function(){
+    const appGrid = document.getElementById("app-grid");
+    appGrid.style.display = "none";
+    const settings = document.getElementById("settings");
+    settings.style.display = "block";
+});
+
+// apps
+appButton.addEventListener("click", function(){
+    const appGrid = document.getElementById("app-grid");
+    appGrid.style.display = "grid";
+    const settings = document.getElementById("settings");
+    settings.style.display = "none";
+});
